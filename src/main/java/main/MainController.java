be9281f75a13fd817473d8b94d05854eee885e61
unknown <check@nevada.unr.edu>
2018-12-24 14:40:27 -0800

    init commit

---
+++ src/main/java/main/MainController.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ * Created by gandalf.midearth 12/24/2018
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MainController {
+
+    public static void main(String[] args){
+        SpringApplication.run(MainController.class, args);
+    }
+}
